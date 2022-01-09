1bcba0894daff5a8158761d71702681c1d909ff9
Deven Valisten <deven.valisten@gmail.com>
2022-01-09 19:04:38 +0700

    menambahkan edit activity

---
+++ app/src/main/java/com/uas/bengkel/EditPenyewaActivity.java
@@ -0,0 +1,13 @@
+package com.uas.bengkel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditPenyewaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_penyewa);
+    }
+}
